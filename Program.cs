d1e314cd35e3763859066f75f75a80c2c410e0f5
Natarajan-rangasamy <natraj2431@gmail.com>
2024-08-12 19:50:09 +0530

    initial

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace banking;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Bank of Intrest :)");
+        Console.WriteLine();
+        Console.WriteLine("Enter -----------------\n 1 -> to open bank account \n 2 -> to Login to your account \n 3 -> to Deposit \n 4 -> to withdraw \n 5 -> to check Balance ");
+
+    }
+}
+
